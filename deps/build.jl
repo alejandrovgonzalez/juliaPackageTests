82718909c8b389d17f851086ab2c931981bee853
alejandrovgonzalez <alexvilloria26@gmail.com>
2021-02-05 17:43:15 +0200

    w/e

--- deps/build.jl
+++ deps/build.jl
@@ -10,3 +10,17 @@ end
 if Sys.isunix() && Sys.islinux()
     println("cool!")
 end
+
+git clone https://github.com/quest-kit/QuEST
+mkdir folder1
+mkdir folder2
+if haskey(ENV, "MYTEST")
+    println("Found environment variable")
+    precision = ENV["QuEST_PREC"]
+    touch test.txt
+    echo precision >> test.txt
+    #cmake -DQuEST_PREC=$precision
+else
+    println("Didn't find env var")
+    #cmake
+end
