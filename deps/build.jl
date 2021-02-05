04a5b97d8053eca730e075408156811f792dad86
alejandrovgonzalez <alexvilloria26@gmail.com>
2021-02-05 17:44:25 +0200

    w/e

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,6 @@
 println("Second commit")
 println("Building package")
 arch = Sys.ARCH
-if Symbol(x86_64) == arch
-    error("64")
-else
-    error("Not 64")
-end
 
 if Sys.isunix() && Sys.islinux()
     println("cool!")
