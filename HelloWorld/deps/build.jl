acee0eebaec5249845d09b1e319ae835d0628158
alejandrovgonzalez <alexvilloria26@gmail.com>
2021-02-05 16:32:38 +0200

    Initial

---
+++ HelloWorld/deps/build.jl
@@ -0,0 +1,11 @@
+println("Building package")
+x = Sys.ARCH
+if string(x) == "x86_64"
+    println("64")
+else
+    error("Not 64")
+end
+
+if Sys.isunix() && Sys.islinux()
+    println("cool!")
+end
